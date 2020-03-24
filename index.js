badf9c46ea80f514cd37c67d5f4065a9c679722e
含光 <mobius_pan@yeah.net>
2020-03-24 23:01:25 +0800

    :sparkles: 最长最长回文字符串

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 /**
  * Created by Mobius on 2020/3/24 22:45.
  */
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function ( s ) {
+  let n = s.length;
+  let res = "";
+  let dp = Array.from( new Array( n ), () => new Array( n ).fill( 0 ) );
+  for (let i = n - 1; i >= 0; i --) {
+    for (let j = i; j < n; j ++) {
+      dp[ i ][ j ] = s[ i ] == s[ j ] && ( j - i < 2 || dp[ i + 1 ][ j - 1 ] );
+      if (dp[ i ][ j ] && j - i + 1 > res.length) {
+        res = s.substring( i, j + 1 );
+      }
+    }
+  }
+  return res;
+};
+console.log( longestPalindrome( "aasdfghahg" ) );
