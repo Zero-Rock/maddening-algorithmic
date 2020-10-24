0fe4eb3f8ac2ad9508c00c09832a70fe72c4b33f
含光 <mobius_pan@yeah.net>
2020-10-24 15:51:12 +0800

    feat: 数字翻转

---
+++ src/7.整数反转.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by 含光<mobius_pan@yeah.net> on 2020/10/24 3:30 下午.
+ * https://leetcode-cn.com/problems/reverse-integer/
+ */
+import { Num } from './common/enum';
+
+const reverse = (num: number): number => {
+  let result = 0;
+  let temp = num;
+  while (temp !== 0) {
+    result = result * Num.ten + temp % Num.ten;
+    temp = (temp / Num.ten) | 0;
+  }
+  return (result | 0) === result ? result : 0;
+};
