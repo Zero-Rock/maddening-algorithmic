32a3dc8ceb1018bfbffe7cf6e336cb5731080437
Zero <mobius_pan@yeah.net>
2022-06-16 23:22:29 +0800

    feat: add validparenthese

---
+++ src/algorithmic/0002.两数相加.ts
@@ -0,0 +1,38 @@
+/**
+ * Created by Zero<mobius_pan@yeah.net> on 2022/6/16 23:02.
+ * @https://leetcode.cn/problems/add-two-numbers/
+ */
+
+class ListNode {
+  val: number
+  next: ListNode | null
+
+  constructor(val?: number | null, next?: ListNode | null) {
+    this.val = (val == undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+const addTwoNumbers = (l1: ListNode | null, l2: ListNode | null): ListNode | null => {
+  const result = new ListNode(null);
+  let next = result;
+  let carryNum = 0;
+  let currSum = 0;
+  let num1 = 0;
+  let num2 = 0;
+
+  while (l1 !== null || l2 !== null) {
+    num1 = l1 !== null ? l1.val : 0;
+    num2 = l2 !== null ? l2.val : 0;
+    currSum = (num1 + num2 + carryNum) % 10;
+    carryNum = Math.floor((num1 + num2 + carryNum) / 10);
+    next.next = new ListNode(currSum);
+    next = next.next;
+    l1 = l1 !== null ? l1.next : null;
+    l2 = l2 !== null ? l2.next : null;
+  }
+  if (carryNum) {
+    next.next = new ListNode(carryNum);
+  }
+  return result.next;
+};
