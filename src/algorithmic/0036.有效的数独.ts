a10689e83bfd09925e919b8d0e2b522aae1d7774
Zero <mobius_pan@yeah.net>
2023-09-02 22:44:37 +0800

    test: update test case

--- src/algorithmic/0036.有效的数独.ts
+++ src/algorithmic/0036.有效的数独.ts
@@ -7,7 +7,7 @@ import { Num } from './common/enum';
 const isValidSudoku = (board: string[][]): boolean => {
   const row: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   const col: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  const sqre: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const square: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   for (let rIndex = 0; rIndex < Num.nine; rIndex++) {
     for (let cIndex = 0; cIndex < Num.nine; cIndex++) {
       if (board[rIndex][cIndex] === '.') {
@@ -16,13 +16,13 @@ const isValidSudoku = (board: string[][]): boolean => {
       const ele: number = Number(board[rIndex][cIndex]);
       const sIndex: number =
         ((rIndex - (rIndex % Num.three)) / Num.three) * Num.three + (cIndex - (cIndex % Num.three)) / Num.three;
-      if ((row[rIndex] >> ele) & 1 || (col[cIndex] >> ele) & 1 || (sqre[sIndex] >> ele) & 1) {
+      if ((row[rIndex] >> ele) & 1 || (col[cIndex] >> ele) & 1 || (square[sIndex] >> ele) & 1) {
         return false;
       } else {
         const temp = 1 << ele;
         row[rIndex] += temp;
         col[cIndex] += temp;
-        sqre[sIndex] += temp;
+        square[sIndex] += temp;
       }
     }
   }
