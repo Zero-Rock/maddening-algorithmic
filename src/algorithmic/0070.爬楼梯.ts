90a638ff3bb4f2afdfe105972ed25b7a9d83ab5d
Zero <mobius_pan@yeah.net>
2025-09-06 14:12:23 +0800

    feat: climbStairs
    Signed-off-by: Zero <mobius_pan@yeah.net>

---
+++ src/algorithmic/0070.爬楼梯.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=typescript
+ *
+ * [70] 爬楼梯
+ */
+const climbStairs = (n: number): number => {
+  if (n <= 2) {
+    return n;
+  }
+  // n-2 层的走法
+  let a = 1;
+  // n-1 层的走法
+  let b = 2;
+  for (let i = 3; i <= n; i++) {
+    a = b;
+    b = a + b;
+  }
+  return b;
+};
